b034e452871f45c3da36946e1138927370a67de9
my-rh-r1020 <myrh.comp@gmail.com>
2020-10-21 13:45:56 +0700

    Merubah print

--- index.py
+++ index.py
@@ -1,11 +1,12 @@
 # File ini untuk Testing GIT Remote
 
 name = input('Enter Your Name : ')
-print('Hello,', name, '\nHow are you today?')
+print('Hello,', name, '\nHow are you feeling today?')
 
 feel = input()
 print('Well great for you ^_^', name)
 
 print('How about your grade in previous semester?')
 grade = float(input())
-print('Awesome! I hope you can maintain your grade for next semester')
+print('Awesome! You got ', grade,
+      'I hope you can maintain your grade for next semester')
