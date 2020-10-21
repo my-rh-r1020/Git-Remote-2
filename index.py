feaa0a6125d8123faaf549c100cce4510bfae7e3
Rifaldi09 <rifaldiherikson@gmail.com>
2020-10-21 12:46:04 +0700

    Menambahkan file index.py

---
+++ index.py
@@ -0,0 +1,11 @@
+# File ini untuk Testing GIT Remote
+
+name = input('Enter Your Name : ')
+print('Hello,', name, '\nHow are you today?')
+
+feel = input()
+print('Well great for you ^_^', name)
+
+print('How about your grade in previous semester?')
+grade = float(input())
+print('Awesome! I hope you can maintain your grade for next semester')
